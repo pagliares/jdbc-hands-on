e0a3eaa471d0f8ee270e06102803ecb630a3eb31
Rodrigo Martins Pagliares <pagliares@Rodrigos-MacBook-Pro.local>
2022-10-11 16:18:09 -0300

    Create project Java7TryWithResources

---
+++ Source_Code/Java7TryWithResources/src/main/java/xyz/pagliares/jdbc/Main.java
@@ -0,0 +1,33 @@
+package xyz.pagliares.jdbc;
+
+import java.sql.*;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+
+		try (Connection conn = DatabaseUtility.getConnection(RDBMS.MYSQL);
+			 Statement	stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			 ResultSet rs = stmt.executeQuery("SELECT * FROM GUEST");
+		){
+
+			System.out.println("Successfully connected to MySQL!");
+			System.out.println("Executing the query SELECT * FROM GUEST");
+			rs.last();
+			System.out.println("Number of rows returned: " + rs.getRow());
+
+		} catch (SQLException e) {
+			DatabaseUtility.processException(e);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+

20376e441b47cbe40181903561c630f55240f8a8
Rodrigo Martins Pagliares <pagliares@Rodrigos-MacBook-Pro.local>
2022-10-11 16:27:33 -0300

    Fix JDBCExceptionHandling undoing some undesired modifications of a previous commit

--- Source_Code/JDBCExceptionHandling/src/main/java/xyz/pagliares/jdbc/Main.java
+++ Source_Code/JDBCExceptionHandling/src/main/java/xyz/pagliares/jdbc/Main.java
@@ -6,18 +6,30 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 
-		try (Connection conn = DatabaseUtility.getConnection(RDBMS.MYSQL);
-			 Statement	stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-			 ResultSet rs = stmt.executeQuery("SELECT * FROM GUEST");
-		){
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 
-			System.out.println("Successfully connected to MySQL!");
-			System.out.println("Executing the query SELECT * FROM GUEST");
+		try {
+			conn = DatabaseUtility.getConnection(RDBMS.MYSQL);
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery("SELECT GUEST_ID, FIRST_NAME FROM GUEST");
+
 			rs.last();
-			System.out.println("Number of rows returned: " + rs.getRow());
+			System.out.println("Number of rows: " + rs.getRow());
 
 		} catch (SQLException e) {
 			DatabaseUtility.processException(e);
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
 		}
 	}
 }

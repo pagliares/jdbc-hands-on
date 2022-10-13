e3bdc7b6b995d5489b6721e8250f4bae4b9d2d0b
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-10-13 18:51:15 -0300

    Create example JavaBeanUpdateSQLUpdatePreparedStatement

---
+++ Source_Code/JavaBeanUpdateSQLUpdatePreparedStatement/src/main/java/xyz/pagliares/jdbc/util/KeyboardInput.java
@@ -0,0 +1,22 @@
+package xyz.pagliares.jdbc.util;
+
+import java.util.Scanner;
+
+public class KeyboardInput {
+    public static String readInputAsString(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public static double readInputAsDouble(String prompt) throws NumberFormatException {
+        String input = readInputAsString(prompt);
+        return Double.parseDouble(input);
+
+    }
+
+    public static int readInputAsInteger(String prompt) throws NumberFormatException {
+        String input = readInputAsString(prompt);
+        return Integer.parseInt(input);
+    }
+}
